3ab2df6065545f8df25394b3c47deec3cb3a4131
Spencer Whitehead <spencerwhitehead@Spencers-MacBook-Pro.local>
2016-10-10 14:24:42 -0400

    Updated Node.java file

--- src/Node.java
+++ src/Node.java
@@ -5,14 +5,14 @@ import java.net.Socket;
 import java.util.*;
 
 public class Node implements Runnable {
-    int portNum;	//Port Number on which the Node will be Listening to accept connection
+    int portNum;	//Port Number on which the Node.java will be Listening to accept connection
     int ID;	        //NodeID of the node
     HashMap neighbors = new HashMap(); // Neighbor node IDs and IP addresses (nodeID, IP)
     HashMap holders = new HashMap(); // Holders of each file (filename, holderID)
     HashMap asked = new HashMap(); // Map to store which files have been requested (filename, asked)
     HashMap state = new HashMap(); // Map to store which files are being used (filename, inUse)
 
-    public Node(int port, int ident, HashMap<int,String> neigh) {
+    public Node(int port, int ident, HashMap<Integer,String> neigh) {
         this.portNum = port;
         this.ID = ident;
         this.neighbors = neigh;
@@ -37,7 +37,7 @@ public class Node implements Runnable {
 //                readFile(String fname);
                 break;
             case "append":
-                if contents != null {
+                if(contents != null) {
                     System.out.println("Appending "+contents+" to file");
                 }
 //                appendFile(String fname, contents);
@@ -64,11 +64,12 @@ public class Node implements Runnable {
 
     public static void main(String[] args) throws Exception {
         Node n = new Node(4444, 1, null);
-        Scanner scan = new Scanner(System.in);
-        String com = scan.next();
-        while(!com.equals("quit")){
-            n.takeCommand(com);
-            scan.next();
-        }
+        System.out.println("HELLO");
+//        Scanner scan = new Scanner(System.in);
+//        String com = scan.next();
+//        while(!com.equals("quit")){
+//            n.takeCommand(com);
+//            scan.next();
+//        }
     }
 }
